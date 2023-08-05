aa3b5b336e5d69eab24a21e2f7f7ae2180ff94bb
Samiullah Kalhoro <91752628+SAMIULLAH-KALHORO-DEV@users.noreply.github.com>
2023-08-05 23:09:35 +0500

    uploading Project

---
+++ android/app/src/main/kotlin/com/example/online_academy_ui_design/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.online_academy_ui_design
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+import 'home.dart';
+
+void main(List<String> args) {
+  runApp(MyApp());
+}
+
+
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+  
+      home: Home(),
+    );
+  }
+}
