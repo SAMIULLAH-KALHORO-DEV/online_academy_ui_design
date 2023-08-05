835a3ca8f11b29647484a475173d7f91c23fe23c
Samiullah Kalhoro <91752628+SAMIULLAH-KALHORO-DEV@users.noreply.github.com>
2023-08-06 00:15:31 +0500

    for performance imporvement

--- lib/main.dart
+++ lib/main.dart
@@ -14,7 +14,7 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(useMaterial3: true),
-      home: Home(),
+      home: const Home(),
     );
   }
 }
